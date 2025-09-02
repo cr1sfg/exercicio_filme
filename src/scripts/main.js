d62aca7d0fb8b4d8e0fa30ed1fa8359cb803437a
Cristiana Goncalves <cristianag61@gmail.com>
2025-09-02 16:59:10 -0300

    Melhorando código e erro javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,17 +6,6 @@ document.addEventListener('DOMContentLoaded', function(){
     const heroSection = document.querySelector('.hero');
     const alturaHero = heroSection.clientHeight;
 
-    window.addEventListener('scroll', function(){
-        const posicaoAtual = window.scrollY;
-
-        if (posicaoAtual < alturaHero) {
-            ocultaElementosDoHeader();
-        } else {
-            exibeElementosDoHeader();
-        }
-    })
-
-
     // Seção de atrações, programação das abas
     for (let i =0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
@@ -32,17 +21,6 @@ document.addEventListener('DOMContentLoaded', function(){
     //Seção FAQ, accordion
     for(let i = 0; i < quenstions.length; i++) {
         quenstions[i].addEventListener('click', abreOuFechaResposta);
-    }
-
-    function ocultaElementosDoHeader() {
-        const header = document.querySelector('header');
-        header.classList.add('header--is-hidden');
-    }
-
-    function exibeElementosDoHeader() {
-        const header = document.querySelector('header');
-        header.classList.remove('header--is-hidden');
-
     }
 
     function abreOuFechaResposta(elemento) {
